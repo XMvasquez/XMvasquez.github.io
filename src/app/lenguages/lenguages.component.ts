fab510bc8807144858c37c96d62515b22998a48e
XMvasquez <ximenavasquez3410@gmail.com>
2022-09-09 11:08:17 -0600

    componentes creados

---
+++ src/app/lenguages/lenguages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lenguages',
+  templateUrl: './lenguages.component.html',
+  styleUrls: ['./lenguages.component.css']
+})
+export class LenguagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
